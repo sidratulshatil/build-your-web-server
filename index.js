3d5fd88d49f32dc03845fa9435bb4b48151630fb
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-10-25 11:22:02 +0600

    Three course details added

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
+const courses = require('./data/course-details.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Build your web server running')
 })
-
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 app.listen(port, () => {
     console.log('Server running on 5000 port')
 })
